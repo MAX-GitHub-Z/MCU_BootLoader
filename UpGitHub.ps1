17fb76711c5ed75ce0cdb996b743f9abdb7b7233
MAX-GitHub-Z <3014759318@qq.com>
2024-08-04 15:37:11 +0800

    test up

--- UpGitHub.ps1
+++ UpGitHub.ps1
@@ -16,15 +16,16 @@ Function gitPush {
 
     Set-Location $ScriptDir # 切换到项目路径
     $now = Get-Date # 获取时间日期对象
-    $msg = "==> " + $now.ToString('yyyy-MM-dd HH:mm:ss') + " commit <=="# 提交的消息
+	$age = Read-Host -Prompt "输入提交信息"
+    $msg = "==>当前时间 " + $now.ToString('yyyy-MM-dd HH:mm:ss') + " commit $age <=="# 提交的消息
     # 将提交信息输出到日志文件gitpush.log
     $msg | Out-File -FilePath .\gitpush.log -Append -Encoding utf8
     Write-Output $msg
-    # 将代码的相关信息输出到日志文件gitpush.log
+	# 将代码的相关信息输出到日志文件gitpush.log
     git status >> .\gitpush.log 
     git add . >> .\gitpush.log
-    git commit -m $msg  >> .\gitpush.log
-    git push origin main  >> .\gitpush.log
+    git commit -m "$age"  >> .\gitpush.log
+    git push origin master  >> .\gitpush.log
 }
 
 gitPush # 运行函数
