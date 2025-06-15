0a25f65c7c290608d4ab3f7630c8e0f3d6b7ffa2
MAX-GitHub-Z <3014759318@qq.com>
2025-06-15 21:22:30 +0800

    删除多余文件

--- project/Objects/startup_stm32f10x_hd.d
+++
@@ -1,1 +0,0 @@
-.\objects\startup_stm32f10x_hd.o: ..\CPU_LIB\STM32F10X\CMSIS\startup\startup_stm32f10x_hd.s
--- project/Objects/startup_stm32f10x_ld.d
+++
@@ -1,1 +0,0 @@
-.\objects\startup_stm32f10x_ld.o: ..\CPU_LIB\STM32F10X\CMSIS\startup\startup_stm32f10x_ld.s
